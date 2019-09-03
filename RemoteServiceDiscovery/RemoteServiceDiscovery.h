15e7b0007d83beee42929bd61e14fb599566dd25
Aun-Ali Zaidi <admin@kodeit.net>
2019-09-03 08:16:50 -0500

    Initial Commit

---
+++ RemoteServiceDiscovery/RemoteServiceDiscovery.h
@@ -0,0 +1,19 @@
+//
+//  RemoteServiceDiscovery.h
+//  RemoteServiceDiscovery
+//
+//  Created by Aun-Ali Zaidi on 9/3/19.
+//  Copyright © 2019 Aun-Ali Zaidi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RemoteServiceDiscovery.
+FOUNDATION_EXPORT double RemoteServiceDiscoveryVersionNumber;
+
+//! Project version string for RemoteServiceDiscovery.
+FOUNDATION_EXPORT const unsigned char RemoteServiceDiscoveryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteServiceDiscovery/PublicHeader.h>
+
+
---
+++ RemoteServiceDiscoveryTests/RemoteServiceDiscoveryTests.m
@@ -0,0 +1,37 @@
+//
+//  RemoteServiceDiscoveryTests.m
+//  RemoteServiceDiscoveryTests
+//
+//  Created by Aun-Ali Zaidi on 9/3/19.
+//  Copyright © 2019 Aun-Ali Zaidi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RemoteServiceDiscoveryTests : XCTestCase
+
+@end
+
+@implementation RemoteServiceDiscoveryTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
